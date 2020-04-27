c2e8f170d97f5ff1d1d3db7c7dcdd82205f594ce
Andrew Thomas <athomik79@gmail.com>
2020-04-27 17:27:14 -0400

    addEmployee function working, updating new employee in db

--- app.js
+++ app.js
@@ -41,7 +41,44 @@ async function employeeSummary() {
     if (err) throw err;
     console.table(res);
     runApp();
-  });
+  });f
+}
+
+async function addEmployee() {
+  let positions = await db.query('Select id, title FROM role');
+  let managers = await db.query('Select id, CONCAT(first_name, " ", last_name) AS Manager FROM employee');
+  managers.unshift({id: null, Manager: "None"});
+
+  inquirer
+    .prompt([{
+      name:"firstName",
+      type: "input",
+      message: "Enter employee's first name:"
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "Enter employee's last name:"
+    },
+    {
+      name: "role",
+      type: "list",
+      message: "Choose employee role:",
+      choices: positions.map(obj => obj.title)
+    },
+    {
+      name: "manager",
+      message:"Choose a manager for the employee:",
+      choices: managers.map(obj => obj.Manager)
+    }
+    ]).then((answers) => {
+      let positionDetails = positions.find(obj => obj.title === answers.role);
+      let manager = managers.find(obj => obj.Manager === answers.manager);
+      db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)",
+      [[answers.firstName, answers.lastName, positionDetails.id,manager.id]]);
+      console.log(`${answers.firstName} was added to the employee database`);
+      runApp();
+    });
 }
 
 function runApp() {
@@ -64,6 +101,7 @@ function runApp() {
           employeeSummary();
           break;
         case "Add a New Employee":
+          addEmployee();
           break;
         case "Update Employee Info":
           break;
--- db/seed.sql
+++ db/seed.sql
@@ -7,4 +7,4 @@ INSERT INTO role (title, salary, department_id)
 VALUES ('Sales Lead', 100000, 1), ('Salesperson', 80000, 1), ('Lead Engineer', 150000, 2), ('Software Engineer', 120000, 2), ('Accountant', 125000, 3), ('Legal Team Lead', 250000, 4), ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES ('Cali', 'Bear', 1, null), ('Mellow', 'Bud', 3, null), ('Thor', 'Gato', 4, 2), ('Rudy', 'Begas', 6, null), ('Barry', 'Badrinath', 2, 1), ('Frank', 'Dux', 2, 1);
+VALUES ('Cali', 'Bear', 1, null), ('Mellow', 'Bud', 3, null), ('Thor', 'Gato', 4, 2), ('Rudy', 'Begas', 6, null), ('Barry', 'Badrinath', 2, 1), ('Frank', 'Dux', 7, 4);
