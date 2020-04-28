f8f2e1e1411aefae61353cbab8d97caaf5a24c64
Andrew Thomas <athomik79@gmail.com>
2020-04-28 19:50:20 -0400

    remove employee function working, added README.md from homework files for easy access

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ async function employeeSummary() {
     if (err) throw err;
     console.table(res);
     runApp();
-  });f
+  });
 }
 
 async function addEmployee() {
@@ -68,6 +68,7 @@ async function addEmployee() {
     },
     {
       name: "manager",
+      type: "list",
       message:"Choose a manager for the employee:",
       choices: managers.map(obj => obj.Manager)
     }
@@ -79,33 +80,56 @@ async function addEmployee() {
       console.log(`${answers.firstName} was added to the employee database`);
       runApp();
     });
-}
+};
+
+  async function removeEmployee () {
+    let employees = await db.query('Select id, CONCAT(first_name, " ", last_name) AS name FROM employee');
+    employees.push({id: null, name: "Cancel"});
+
+    inquirer
+      .prompt([
+        {
+          name: "employeeName",
+          type: "list",
+          message: "Which employee would you like to remove?",
+          choices: employees.map(obj => obj.name)
+        }
+      ]).then((response) => {
+        if (response.employeeName != "Cancel") {
+          let exEmployee = employees.find(obj => obj.name === response.employeeName);
+          db.query("DELETE FROM employee WHERE id=?", exEmployee.id);
+          console.log(`${response.employeeName} is no longer an employee`);
+        }
+        runApp();
+      })
+  };
 
 function runApp() {
   inquirer
     .prompt({
-      name: "employeetrack",
+      name: "employeetracker",
       type: "list",
       message: "What would you like to do?",
       choices: [
         "View All Employees",
         "Add a New Employee",
-        "Update Employee Info",
         "Remove an Employee",
+        "Update Employee Info",
         "Add a New Role",
         "Add a New Department"
       ]
     }).then((responses) => {
-      switch (responses.employeetrack) {
+      switch (responses.employeetracker) {
         case "View All Employees":
           employeeSummary();
           break;
         case "Add a New Employee":
           addEmployee();
           break;
-        case "Update Employee Info":
-          break;
         case "Remove an Employee":
+          removeEmployee();
+          break;
+        case "Update Employee Info":
           break;
         case "Add a New Role":
           break;
